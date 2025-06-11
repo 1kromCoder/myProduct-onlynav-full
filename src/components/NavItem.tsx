ff7ad689a73ad27aa3226168c6a97b7a092cf939
Ikromxon <ikromkhanshukrullayev03022005@gmail.com>
2025-06-11 14:11:05 +0500

    okee

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -4,7 +4,7 @@ const NavItem: FC<{ title: string }> = ({ title }) => {
   return (
     <li>
       <a
-        className="hover:text-[var(--clr-light-green)] duration-300 font-medium text-[16px] text-[var(--clr-text)] leading-[28px]"
+        className="hover:text-red-500 duration-300 font-medium text-[16px] text-[var(--clr-text)] leading-[28px]"
         href="#"
       >
         {title}
